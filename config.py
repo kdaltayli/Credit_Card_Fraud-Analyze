806472f2e5f14983d57aa39767090f1a789af812
Kursad Altayli <kdaltayli@gmail.com>
2020-11-20 17:42:40 -0500

    last update

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-username = "rhmphjkssxvhxs"
-passowrd = "2ff88b3da4fa0b6f2f421d7b5aa5361447957c22ad5d73ffb979bbeb47007aaf"
+username = "wkbqaamsvirbvv"
+password = "977539a35f5473222775a21aab8fbd40c6c5744fcc74cffa89fd7c217a18e720"
+dbname = "dfn8eu8kn4rk3j"
+host = "ec2-34-202-65-210.compute-1.amazonaws.com"
