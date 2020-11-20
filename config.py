b28e6310d2db4ab514eb3b68bfbe446396988acc
Kursad Altayli <kdaltayli@gmail.com>
2020-11-20 17:59:54 -0500

    updated for confipy

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "wkbqaamsvirbvv"
-password = "977539a35f5473222775a21aab8fbd40c6c5744fcc74cffa89fd7c217a18e720"
-dbname = "dfn8eu8kn4rk3j"
-host = "ec2-34-202-65-210.compute-1.amazonaws.com"
+username = "write your username"
+password = "use your password"
+dbname = "use your databasename"
+host = "use your host "
